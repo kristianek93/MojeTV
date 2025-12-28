abe26ac5f7412b6fe020b3c0419c837441bc7ea0
kristianek93 <60257741+kristianek93@users.noreply.github.com>
2025-12-28 01:37:58 +0100

    Add files via upload

---
+++ core.js
@@ -0,0 +1,8 @@
+﻿console.log("GitHub Core: Startuji...");
+var fakeUA = 'Mozilla/5.0 (Linux; Tizen 2.4.0) AppleWebKit/537.3 (KHTML, like Gecko) SamsungBrowser/1.1 TV Safari/537.3';
+try { Object.defineProperty(navigator, 'userAgent', { get: function() { return fakeUA; } }); } catch(e) {}
+
+setTimeout(function() {
+    console.log("Jdu na YouTube...");
+    window.location.replace("https://www.youtube.com/tv");
+}, 1000);
